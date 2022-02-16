0f62181b615f6533bc4b71423d848dd1587e22f7
wajeshubham <waje.shubham111@gmail.com>
2022-02-16 20:41:59 +0530

    added regex and validations and ran test cases

--- test/index.js
+++ test/index.js
@@ -1,9 +1,12 @@
 import validator from "../index.js";
 
-console.log("Mobile Validation : ", validator.isMobileNumberValid());
-console.log("Pin Validation : ", validator.isPINValid());
-console.log("OTP Validation : ", validator.isOTPValid());
-console.log("PAN Card Validation : ", validator.isPANCardValid());
-console.log("Age Validation : ", validator.isAgeValid());
-console.log("Email Validation : ", validator.isEmailValid());
-console.log("Website Validation : ", validator.isWebsiteValid());
+console.log(
+  "Mobile Validation : ",
+  validator.isMobileNumberValid(9283383383, 91)
+);
+console.log("Pin Validation : ", validator.isPINValid(849993));
+console.log("OTP Validation : ", validator.isOTPValid(478394));
+console.log("PAN Card Validation : ", validator.isPANCardValid("ABCDE1234F"));
+console.log("Age Validation : ", validator.isAgeValid("07-23-2003", 18));
+console.log("Email Validation : ", validator.isEmailValid("abcd@h.com"));
+console.log("Website Validation : ", validator.isWebsiteValid("abc.com"));
