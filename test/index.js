6b100f25845743eff27da3820e28be1091580aaa
Chetan Kantharia <chetan.kantharia@gmail.com>
2022-02-16 17:57:45 +0530

    Initial Commit

---
+++ test/index.js
@@ -0,0 +1,9 @@
+import validator from '../index.js';
+
+console.log('Mobile Validation : ', validator.isMobileNumberValid());
+console.log('Pin Validation : ', validator.isPINValid());
+console.log('OTP Validation : ', validator.isOTPValid());
+console.log('PAN Card Validation : ', validator.isPANCardValid());
+console.log('Age Validation : ', validator.isAgeValid());
+console.log('Email Validation : ', validator.isEmailValid());
+console.log('Website Validation : ', validator.isWebsiteValid());
